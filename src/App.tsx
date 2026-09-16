da18b106d7c8555e60f5675669869f7c95a1aaf2
fran <lopezfrancisco1506@gmail.com>
2026-09-16 16:13:28 -0500

    feat: Creación de primeros componentes y hooks para probar visualizacion del api
    Se crearon los componentes de character card, character list y status badge, se creó el hook para hacer el fetch de los personajes, hizo un mockup de App.ts y su respectivo css

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,48 @@
 import { useState } from 'react'
-
+import CharacterList from './components/CharacterList'
+import useCharacters  from './hooks/useCharacters'
+import styles from './App.module.css'
 
 function App() {
-  
+  const [query, setQuery] = useState('Rick')
+  const [selectedId, setSelectedId] = useState<number | null>(null)
+  const [page, setPage] = useState(1)
+
+  const { characters, loading, error, isEmpty, totalPages } = useCharacters(query, page)
+
+  function renderList() {
+    if (loading){
+      return <p>Cargando...</p>
+    }
+    if (error) {
+      return <p>Error: {error}</p>
+    }
+    if (isEmpty) {
+      return <p>No hay personajes que coincidan con tu búsqueda.</p>
+    }
+    return (
+      <CharacterList
+        characters={characters}
+        onSelect={setSelectedId}
+      />
+    )
+  }
 
   return (
-    <>
-      Hola
-    </>
+    <div className= {styles.app}>
+      <header className={styles.header}>
+        <div className={styles.headerInner}>
+          <h1 className={styles.title}>
+            Rick <span className={styles.accent}> & </span> Morty
+          </h1>
+        </div>
+      </header>
+
+      <main className={styles.main}>
+        {renderList()}
+      </main>
+
+    </div>
   )
 }
 
